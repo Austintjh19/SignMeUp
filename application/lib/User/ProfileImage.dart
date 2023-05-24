84a7c0bc1d9ef081a2f1761aa639dfe2ebb418b8
Austintjh19 <AustinTJH19@gmail.com>
2023-05-24 22:31:57 +0800

    Profile Picture Half Done

--- application/lib/User/ProfileImage.dart
+++ application/lib/User/ProfileImage.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ProfileImage extends StatefulWidget {
   const ProfileImage({
@@ -10,20 +13,49 @@ class ProfileImage extends StatefulWidget {
 }
 
 class _ProfileImageState extends State<ProfileImage> {
+  String profilePicLink = "";
+
+  void pickUploadProfilePic() async {
+    final image = await ImagePicker().pickImage(
+      source: ImageSource.gallery,
+      maxHeight: 512,
+      maxWidth: 512,
+      imageQuality: 90,
+    );
+
+    Reference ref = FirebaseStorage.instance.ref().child("profilepic.jpg");
+
+    await ref.putFile(File(image!.path));
+
+    ref.getDownloadURL().then((value) async {
+      setState(() {
+        profilePicLink = value;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     double width = MediaQuery.of(context).size.width;
-    return CircleAvatar(
-      radius: width * 0.25,
-      child: const SizedBox.expand(
-        child: FittedBox(
-          fit: BoxFit.fill,
-          child: Icon(
-            Icons.person_2_rounded,
-            color: Colors.white,
-          ),
-        ),
-      ),
+    return GestureDetector(
+      onTap: pickUploadProfilePic,
+      child: profilePicLink == ""
+          ? CircleAvatar(
+              radius: width * 0.25,
+              child: const SizedBox.expand(
+                child: FittedBox(
+                  fit: BoxFit.fill,
+                  child: Icon(
+                    Icons.person_2_rounded,
+                    color: Colors.white,
+                  ),
+                ),
+              ),
+            )
+          : ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: Image.network(profilePicLink),
+            ),
     );
   }
 }
