7a6dabe874237f557c10c9f5f00d3bc1a6a439ee
Austintjh19 <AustinTJH19@gmail.com>
2023-06-05 22:55:06 +0800

    Removal

--- application/lib/src/features/authentication/controllers/ForgotPasswordController.dart
+++ application/lib/src/features/authentication/controllers/ForgotPasswordController.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:myapplication/src/repository/authentication_repository/AuthenticationRepository.dart';
-import 'package:myapplication/src/repository/user_repository/UserRepository.dart';
 
 class ForgotPasswordController extends GetxController {
   static ForgotPasswordController get instance => Get.find();
