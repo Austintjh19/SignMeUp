1e9831e407612b9f442cfe49fa5a7e7df4f89f4a
Austintjh19 <AustinTJH19@gmail.com>
2023-07-04 13:54:07 +0800

    Added more fields to Event Model

--- application/lib/src/repository/event_repository/EventRepository.dart
+++ application/lib/src/repository/event_repository/EventRepository.dart
@@ -77,6 +77,7 @@ class EventRepository extends GetxController {
     return eventsCollection;
   }
 
+  //
   Future<List<EventModel>> searchEvent(
       String query, String filterBy, bool isDescending) async {
     List<EventModel> eventsCollection = [];
